bffef0625f386ccec16ff6ca1f89421c4756f997
TheOrangeCow <da18foster@gmail.com>
2026-09-14 17:25:10 +0100

    update

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('POST_SECRET_KEY', 'dev-secret-key-change-me')
+app.secret_key = os.environ.get('POST_SECRET_KEY')
 
 DB_PATH = os.path.join(os.path.dirname(__file__), 'mail.db')
 
